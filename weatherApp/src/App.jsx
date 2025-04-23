6e18c130a9dc457933cf5f26c975a8c015efee5f
Utpanna Pradhan <Utpanna828@gmail.com>
2025-04-23 16:58:52 +0530

    initial-commit

---
+++ weatherApp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home'
+
+function App() {
+ 
+
+  return (
+  <div className='d-flex flex-row justify-content-center align-items-center'>
+  <Home />
+  </div>
+  )
+}
+
+export default App
